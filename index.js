9b4549a6e99106bfee64a4628ea28fb6cb28d5a2
RubberDuck55 <woodjb17@gmail.com>
2022-08-03 21:58:27 -0400

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+// CORS makes me sad sometimes :(
+let emoji = {
+    emoji: ["❤️","👍","😎","💥","⭐","🔥","✔️","❗","❌","🚀","🌈"]
+};
+// fetch("data/emoji.json")
+// .then(response => {
+//    emoji = response.json();
+// });
+
+let ea = emoji.emoji;
+
