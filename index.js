5c3cad9e0e76f50a783b275bc88b919838ba26c1
James <110496446+RubberDucky5@users.noreply.github.com>
2022-08-03 23:02:22 -0400

    🚀 Version 1.0 Complete!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // CORS makes me sad sometimes :(
 let emoji = {
-    emoji: ["❤️","👍","😎","💥","⭐","🔥","✔️","❗","❌","🚀","🌈"]
+    emoji: ["❤️","👍","😎","💥","⭐","🔥","✔️","❗","❌","🚀","🌈","🐞"]
 };
 // fetch("data/emoji.json")
 // .then(response => {
@@ -8,4 +8,18 @@ let emoji = {
 // });
 
 let ea = emoji.emoji;
+let emojiButtonList = [];
 
+for(let i = 0; i < ea.length; i++){
+  let temp = document.createElement("button");
+  temp.classList.add("copy-button");
+  temp.innerHTML = ea[i];
+  emojiButtonList.push(temp);
+  temp.addEventListener('mousedown', (e) => {
+    if(e.button != 0)
+      return;
+
+    navigator.clipboard.writeText(ea[i]);
+  });
+  document.body.appendChild(emojiButtonList[i]);
+}
